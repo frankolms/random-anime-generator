8f1d7c7c4737b3f7be29e5ee8bbedeb918c7eee7
frankolms <frankolms@gmail.com>
2023-10-20 21:41:36 -0400

    image of random anime now displays

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 const randomAnime = document.getElementById("randomAnime");
 const title = document.getElementById("animeTitle");
+const image = document.getElementById("animeImg");
 
 randomAnime.addEventListener("click", () => {
   fetch("https://api.jikan.moe/v4/random/anime")
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      displayAnimeTitle(data);
+      displayAnimeInfo(data);
     });
 });
 
-displayAnimeTitle = (data) => {
+displayAnimeInfo = (data) => {
   if (data.data.title_english) {
     title.textContent = data.data.title_english;
   } else {
     title.textContent = data.data.title;
   }
+
+  image.src = data.data.images.jpg.image_url;
 };
