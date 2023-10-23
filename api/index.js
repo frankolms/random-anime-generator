0511a0e72abaec926be58f62ac321a8090b7d6ff
frankolms <frankolms@gmail.com>
2023-10-23 19:55:58 -0400

    now using official MAL API instead of third party api, created proxy server and deployed to vercel to access api in the browser

---
+++ api/index.js
@@ -0,0 +1,23 @@
+// export default async function handler(req, res) {
+//   res.status(200).send("Hello World!");
+// }
+
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const apiProxy = createProxyMiddleware({
+  target: "https://api.myanimelist.net/",
+  changeOrigin: true,
+  pathRewrite: {
+    "^/api": "",
+  },
+  onProxyRes(proxyRes) {
+    proxyRes.headers["access-control-allow-origin"] = "*";
+    proxyRes.headers["access-control-allow-methods"] = "GET";
+    proxyRes.headers["access-control-allow-headers"] =
+      "Origin, X-Requested-With, Content-Type, Accept";
+  },
+});
+
+export default function (req, res) {
+  return apiProxy(req, res);
+}
--- main.js
+++ main.js
@@ -2,21 +2,31 @@ const randomAnime = document.getElementById("randomAnime");
 const title = document.getElementById("animeTitle");
 const image = document.getElementById("animeImg");
 
+// randomAnime.addEventListener("click", () => {
+//   fetch("https://api.jikan.moe/v4/random/anime")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data);
+//       displayAnimeInfo(data);
+//     });
+// });
+
+// displayAnimeInfo = (data) => {
+//   if (data.data.title_english) {
+//     title.textContent = data.data.title_english;
+//   } else {
+//     title.textContent = data.data.title;
+//   }
+
+//   image.src = data.data.images.jpg.image_url;
+// };
+
 randomAnime.addEventListener("click", () => {
-  fetch("https://api.jikan.moe/v4/random/anime")
+  fetch("/api/v2/users/frankolms/animelist?status=plan_to_watch&limit=1000", {
+    headers: {
+      "X-MAL-CLIENT-ID": "7293ae0257e63715c16e13f04b73bccd",
+    },
+  })
     .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      displayAnimeInfo(data);
-    });
+    .then((data) => console.log(data));
 });
-
-displayAnimeInfo = (data) => {
-  if (data.data.title_english) {
-    title.textContent = data.data.title_english;
-  } else {
-    title.textContent = data.data.title;
-  }
-
-  image.src = data.data.images.jpg.image_url;
-};
