17385e99a2b43e20e0a16b00083aeb9a2b1da303
frankolms <frankolms@gmail.com>
2023-10-23 20:19:11 -0400

    got rid of commented out code

--- main.js
+++ main.js
@@ -2,25 +2,6 @@ const randomAnime = document.getElementById("randomAnime");
 const title = document.getElementById("animeTitle");
 const image = document.getElementById("animeImg");
 
-// randomAnime.addEventListener("click", () => {
-//   fetch("https://api.jikan.moe/v4/random/anime")
-//     .then((response) => response.json())
-//     .then((data) => {
-//       console.log(data);
-//       displayAnimeInfo(data);
-//     });
-// });
-
-// displayAnimeInfo = (data) => {
-//   if (data.data.title_english) {
-//     title.textContent = data.data.title_english;
-//   } else {
-//     title.textContent = data.data.title;
-//   }
-
-//   image.src = data.data.images.jpg.image_url;
-// };
-
 randomAnime.addEventListener("click", () => {
   fetch("/api/v2/users/frankolms/animelist?status=plan_to_watch&limit=1000", {
     headers: {
