6d2bb10aa8e93f73d0f1fc1b099717bf82bbebee
frankolms <frankolms@gmail.com>
2023-10-25 21:40:48 -0400

    anime scores now display

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ const title = document.getElementById("animeTitle");
 const image = document.getElementById("animeImg");
 const username = document.getElementById("username");
 const animeStatus = document.getElementById("animeStatus");
+const animeScore = document.getElementById("animeScore");
 
 randomAnime.addEventListener("click", () => {
   if (!username.value) {
     alert("Please Enter Your MAL Username");
   } else {
     fetch(
-      `/api/v2/users/${username.value}/animelist?status=${animeStatus.value}&limit=1000`,
+      `/api/v2/users/${username.value}/animelist?fields=mean&status=${animeStatus.value}&limit=1000`,
       {
         headers: {
           "X-MAL-CLIENT-ID": "7293ae0257e63715c16e13f04b73bccd",
@@ -31,4 +32,5 @@ displayAnimeInfo = (data) => {
   console.log(data.data[randomNum]);
   title.textContent = data.data[randomNum].node.title;
   image.src = data.data[randomNum].node.main_picture.medium;
+  animeScore.textContent = data.data[randomNum].node.mean;
 };
